2a7f748fd1376cc6098892fc5a35e60ad69273d7
DfrancoGit <ddelport101@gmail.com>
2025-01-16 12:37:58 +0200

    Added darkmode and fixed icons

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
+const hamburgerSwitch = document.getElementById("hamburger-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+  hamburgerSwitch.innerHTML = "Lightmode";
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+  hamburgerSwitch.innerHTML = "Darkmode";
+};
+
+if (darkmode === "active") {
+  enableDarkmode();
+}
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
+
+const toggleTheme = () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+};
